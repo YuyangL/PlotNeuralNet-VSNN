b373fdcda92da6df212438fbd5e20664a23deb2c
Y Luan <luanyuyang@yahoo.com>
2022-08-23 17:27:23 +0800

    VSNN V1 diagram update 1

--- pycore/blocks.py
+++ pycore/blocks.py
@@ -176,27 +176,6 @@ def block_ConvInception(name, bottom, s_filer=256, n_filer=(64, 128), offset="(1
     ]
 
 
-def block_Unconv(name, bottom, top, s_filer=256, n_filer=64, offset="(1,0,0)", size=(32, 32, 3.5), opacity=0.5):
-    return [
-        to_UnPool(name='unpool_{}'.format(name), offset=offset, to="({}-east)".format(bottom), width=1, height=size[0],
-                  depth=size[1], opacity=opacity),
-        to_ConvRes(name='ccr_res_{}'.format(name), offset="(0,0,0)", to="(unpool_{}-east)".format(name),
-                   s_filer=str(s_filer), n_filer=str(n_filer), width=size[2], height=size[0], depth=size[1],
-                   opacity=opacity),
-        to_Conv(name='ccr_{}'.format(name), offset="(0,0,0)", to="(ccr_res_{}-east)".format(name), s_filer=str(s_filer),
-                n_filer=str(n_filer), width=size[2], height=size[0], depth=size[1]),
-        to_ConvRes(name='ccr_res_c_{}'.format(name), offset="(0,0,0)", to="(ccr_{}-east)".format(name),
-                   s_filer=str(s_filer), n_filer=str(n_filer), width=size[2], height=size[0], depth=size[1],
-                   opacity=opacity),
-        to_Conv(name='{}'.format(top), offset="(0,0,0)", to="(ccr_res_c_{}-east)".format(name), s_filer=str(s_filer),
-                n_filer=str(n_filer), width=size[2], height=size[0], depth=size[1]),
-        to_connection(
-            "{}".format(bottom),
-            "unpool_{}".format(name)
-        )
-    ]
-
-
 def block_UnconvSkipConvInception(name, bottom, s_filer=256, n_filer=(64, 64, 64), offset="(1,0,0)",
                                   size=((32, 32, 5), (32, 32, 3.5), (32, 32, 3.5)),
                                   opacity=0.5, label=('1', '1', '1')):
@@ -204,10 +183,10 @@ def block_UnconvSkipConvInception(name, bottom, s_filer=256, n_filer=(64, 64, 64
         to_UnPool(name='{}-unpool'.format(name), offset=offset, to="({}-east)".format(bottom), width=1,
                   height=size[0][0],
                   depth=size[0][1], opacity=opacity),
-        to_ConvRes(name='{}-res'.format(name), offset="(0,0,0)", to="({}-unpool-east)".format(name),
+        to_ConvRes(name='{}-skip'.format(name), offset="(0,0,0)", to="({}-unpool-east)".format(name),
                    s_filer="", n_filer=str(n_filer[0]), width=size[0][2], height=size[0][0], depth=size[0][1],
                    opacity=opacity, caption="SC{}".format(label[0])),
-        to_Conv(name='{}-c'.format(name), offset="(0,0,0)", to="({}-res-east)".format(name), s_filer="",
+        to_Conv(name='{}-c'.format(name), offset="(0,0,0)", to="({}-skip-east)".format(name), s_filer="",
                 n_filer=str(n_filer[1]), width=size[1][2], height=size[1][0], depth=size[1][1],
                 caption="C{}".format(label[1])),
         to_Inception(
@@ -234,10 +213,10 @@ def block_UnconvSkipConv(name, bottom, s_filer=256, n_filer=(64, 64), offset="(1
         to_UnPool(name='{}-unpool'.format(name), offset=offset, to="({}-east)".format(bottom), width=1,
                   height=size[0][0],
                   depth=size[0][1], opacity=opacity),
-        to_ConvRes(name='{}-res'.format(name), offset="(0,0,0)", to="({}-unpool-east)".format(name),
+        to_ConvRes(name='{}-skip'.format(name), offset="(0,0,0)", to="({}-unpool-east)".format(name),
                    s_filer="", n_filer=str(n_filer[0]), width=size[0][2], height=size[0][0], depth=size[0][1],
                    opacity=opacity, caption="SC{}".format(label[0])),
-        to_Conv(name='{}-c'.format(name), offset="(0,0,0)", to="({}-res-east)".format(name), s_filer=str(s_filer),
+        to_Conv(name='{}-c'.format(name), offset="(0,0,0)", to="({}-skip-east)".format(name), s_filer=str(s_filer),
                 n_filer=str(n_filer[1]), width=size[1][2], height=size[1][0], depth=size[1][1],
                 caption="C{}".format(label[1])),
         to_connection(
